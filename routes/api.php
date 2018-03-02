cc6e02a784630a316e5b321e0abdf7a4964815f8
Ferdie De Oliveira <deferdie@gmail.com>
2018-03-02 16:26:03 +0000

    Using A controller

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('client')->post('/log', function (Request $request) {
-    return json_encode($request->event_content);
-});
+Route::post('/log', 'ProjectExceptionController@store')->middleware('client');
