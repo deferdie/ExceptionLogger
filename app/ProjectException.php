9bd7ca14c38518e65a6be16590722e6e11411ff0
Ferdie De Oliveira <deferdie@gmail.com>
2018-03-02 16:25:07 +0000

    Added new capture fields

--- app/ProjectException.php
+++ app/ProjectException.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectException extends Model
 {
-    //
+    protected $fillable = [
+        'status_code', 
+        'url', 
+        'message', 
+        'headers', 
+        'stack_trace', 
+        'file', 
+        'line_number', 
+        'request_uri', 
+        'server_name',
+        'enviroment', 
+    ];
 }
