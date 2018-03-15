36a8c0643138fc5988ff9beb32a376e48a3318e1
Ferdie De Oliveira <deferdie@gmail.com>
2018-03-15 23:29:03 +0000

    Updated font colours

--- public/js/app.js
+++ public/js/app.js
@@ -18496,8 +18496,9 @@ module.exports = __webpack_require__(64);
 
 /***/ }),
 /* 14 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
 __webpack_require__(15);
 
 var _ = __webpack_require__(6);
